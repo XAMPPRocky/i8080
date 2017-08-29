df82dadd44f3477503692acbc008b047214e6b0c
Aaron Power <theaaronepower@gmail.com>
2017-08-29 02:17:35 +0100

    Cleaned up invaders code

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
-extern crate ears;
-extern crate piston_window;
-
 mod condition_codes;
 mod cpu;
-mod invaders;
 mod memory;
 mod num_impls;
 mod opcode;
@@ -11,6 +7,5 @@ mod pointer;
 mod register;
 
 pub use cpu::*;
-pub use invaders::*;
 pub use opcode::Opcode;
 
